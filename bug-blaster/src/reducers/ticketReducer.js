e85a4be3cd10d44578bbd4a32bdcbcfc64b8fdab
Enache Andrei <enache.andrei.george@gmail.com>
2024-08-04 23:52:03 +0300

    Added update and delete ticket logic in reducer

--- bug-blaster/src/reducers/ticketReducer.js
+++ bug-blaster/src/reducers/ticketReducer.js
@@ -1,3 +1,22 @@
-import { useReducer } from "react";
-
-export default function ticketReducer(state, action) {}
+export default function ticketReducer(state, action) {
+  switch (action.type) {
+    case "ADD_TICKET":
+      return { ...state, tickets: [...state.tickets, action.payload] };
+    case "UPDATE_TICKET":
+      return {
+        ...state,
+        tickets: state.tickets.map((ticket) =>
+          ticket.id === action.payload.id ? action.payload : ticket
+        ),
+      };
+    case "DELETE_TICKET":
+      return {
+        ...state,
+        tickets: state.ticket.filter(
+          (ticket) => ticket.id !== action.payload.id
+        ),
+      };
+    default:
+      return state;
+  }
+}
