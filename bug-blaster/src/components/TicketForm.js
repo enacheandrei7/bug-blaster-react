70816a801bf5405ffa0201c42c05bb94246f4273
Enache Andrei <enache.andrei.george@gmail.com>
2024-08-05 00:00:40 +0300

    Added dispatch function in App.js in order for the ticketForm to be connected to the reducer

--- bug-blaster/src/components/TicketForm.js
+++ bug-blaster/src/components/TicketForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function TicketForm() {
+export default function TicketForm({ dispatch }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("1");
@@ -27,7 +27,10 @@ export default function TicketForm() {
       priority,
     };
 
-    console.log(ticketData);
+    dispatch({
+      type: "ADD_TICKET",
+      payload: ticketData,
+    });
 
     clearForm();
   };
