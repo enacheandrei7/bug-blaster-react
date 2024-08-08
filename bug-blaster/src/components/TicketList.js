3c8af23d64089b289da20e127186d2d654e188a9
Enache Andrei <enache.andrei.george@gmail.com>
2024-08-09 00:20:41 +0300

    Added ticket list where all the tickets will be shown, added Delete and Edit buttons to the tickets, added the delete ticket reducer logic

--- bug-blaster/src/components/TicketList.js
+++ bug-blaster/src/components/TicketList.js
@@ -2,5 +2,15 @@ import React from "react";
 import TicketItem from "./TicketItem";
 
 export default function TicketList({ tickets, dispatch }) {
-  return <div className="ticket-list"></div>;
+  return (
+    <div className="ticket-list">
+      {tickets.map((ticket) => (
+        <TicketItem
+          key={ticket.id}
+          dispatch={dispatch}
+          ticket={ticket}
+        ></TicketItem>
+      ))}
+    </div>
+  );
 }
