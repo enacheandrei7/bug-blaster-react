990bba28f72a2009fa61059cb684c2919ef022a4
Enache Andrei <enache.andrei.george@gmail.com>
2024-08-11 23:30:51 +0300

    Created app that uses AXIOS to get, create, update and delete posts on a mock API.

---
+++ react-api-demo/src/components/PostForm.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import { createPost, updatePost } from "../services/postService";
+
+export default function PostForm({
+  posts,
+  setPosts,
+  editingPost,
+  setEditingPost,
+}) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  useEffect(() => {
+    if (editingPost) {
+      setTitle(editingPost.title);
+      setBody(editingPost.body);
+    } else {
+      setTitle("");
+      setBody("");
+    }
+  }, [editingPost]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (editingPost) {
+      editPost();
+    } else {
+      addPost();
+    }
+    setTitle("");
+    setBody("");
+    setEditingPost(null);
+  };
+
+  const addPost = () => {
+    createPost({ title: title, body: body })
+      .then((res) => setPosts([...posts, res.data]))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const editPost = () => {
+    updatePost(editingPost.id, { title, body })
+      .then((res) =>
+        setPosts(
+          posts.map((post) => (post.id === editingPost.id ? res.data : post))
+        )
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>Title</div>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <div>Body</div>
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      ></textarea>
+      <div>
+        <button type="submit"> {editingPost ? "Edit Post" : "Add Post"}</button>
+      </div>
+    </form>
+  );
+}
